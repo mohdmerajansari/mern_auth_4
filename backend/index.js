ce19ec6447c8926dcd7e6f1625464603521a43ae
Mohd Meraj Ansari <169152384+mohdmerajansari@users.noreply.github.com>
2025-03-01 12:27:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,15 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://mern-auth-4-ui.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use("/ping", (req, res) => {
   res.send("PONG");  
